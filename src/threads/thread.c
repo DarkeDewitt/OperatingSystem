399dd34c08d251d7b0760edffe16e9f58525636c
chshux <624049380@qq.com>
2017-04-09 01:37:55 -0700

    test

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -98,6 +98,8 @@ thread_init (void)
   init_thread (initial_thread, "main", PRI_DEFAULT);
   initial_thread->status = THREAD_RUNNING;
   initial_thread->tid = allocate_tid ();
+
+  initial_thread->ticks_blocked = 0;
 }
 
 /* Starts preemptive thread scheduling by enabling interrupts.
