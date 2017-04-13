8260a46c52baf898df53d469917ff1fba76c9a95
chshux <624049380@qq.com>
2017-04-13 01:56:04 -0700

    alarm-priority

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -247,9 +247,17 @@ thread_unblock (struct thread *t)
 
   old_level = intr_disable ();
   ASSERT (t->status == THREAD_BLOCKED);
-  list_push_back (&ready_list, &t->elem);
+ // list_push_back (&ready_list, &t->elem);
+  list_insert_ordered(&ready_list, &t->elem, (list_less_func *)&more_than, NULL);
   t->status = THREAD_READY;
   intr_set_level (old_level);
+}
+
+bool
+more_than(const struct list_elem *a,
+          const struct list_elem *b,
+          void *aux UNUSED) {
+  return list_entry(a, struct thread, elem) -> priority  > list_entry(b, struct thread, elem) -> priority;
 }
 
 void
@@ -330,7 +338,8 @@ thread_yield (void)
 
   old_level = intr_disable ();
   if (cur != idle_thread) 
-    list_push_back (&ready_list, &cur->elem);
+//  list_push_back (&ready_list, &cur->elem);
+  list_insert_ordered(&ready_list, &cur->elem, (list_less_func *)&more_than, NULL);
   cur->status = THREAD_READY;
   schedule ();
   intr_set_level (old_level);
@@ -483,7 +492,8 @@ init_thread (struct thread *t, const char *name, int priority)
   t->stack = (uint8_t *) t + PGSIZE;
   t->priority = priority;
   t->magic = THREAD_MAGIC;
-  list_push_back (&all_list, &t->allelem);
+//  list_push_back (&all_list, &t->allelem);
+  list_insert_ordered(&all_list, &t->allelem, (list_less_func *)&more_than, NULL);
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
--- src/threads/thread.h
+++ src/threads/thread.h
@@ -119,6 +119,7 @@ tid_t thread_create (const char *name, int priority, thread_func *, void *);
 void thread_block (void);
 void thread_unblock (struct thread *);
 void blocked_thread_check(struct thread *, void *);
+bool more_than(const struct list_elem *, const struct list_elem *, void *aux);
 
 struct thread *thread_current (void);
 tid_t thread_tid (void);
